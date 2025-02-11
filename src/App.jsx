86f06f18d31ada98586c28eada9e084300f8742c
Lokesh Patil <lokeshbpatil2004@gmail.com>
2025-02-11 10:28:30 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Paste from './components/paste'
+import Paste from './components/Paste'
 import ViewPaste from './components/ViewPaste'
 import AdminPage from './components/Admin'
 
